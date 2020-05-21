340409e2525db7f762d5396150602fb873cc1f87
Daniel <damesaa@eafit.edu.co>
2020-05-21 02:56:33 -0500

    The model has 0.75

--- LogisticRegression/CleanData.py
+++ LogisticRegression/CleanData.py
@@ -12,25 +12,12 @@ from random import randint, uniform,random
 import statistics as stats
 #%matplotlib inline
 
-#hvacs_raw = pd.read_excel('./input/tr_hvacs.xlsx')
+
 zones = pd.read_excel('./input/tr_zones.xlsx')
 setPoint = 23.5
 
-'''hvacs =   hvacs_raw[
-    ['Date',
-    'h_Cap_Ch1', 
-    'h_Cap_Ch2', 
-    'h_Cap_ChC', 
-    'h_Cap_ChF'
-    ]
-]'''
-
-
-#df2 = pd.DataFrame()
-#df2 = zones
-#print(df2.count)
 #Selecciona el periodo de observacion
-zones = zones[(zones['Date'] >= '2016-09-11') & (zones['Date'] < '2018-01-01')] 
+zones = zones[(zones['Date'] >= '2016-09-11') & (zones['Date'] < '2018-04-06')] 
 
 #writer = ExcelWriter('s1.xlsx')
 #hvacs.to_excel(writer, 'Hoja de datos', index=False)
@@ -46,13 +33,16 @@ writer.save()"""
 zones = zones.set_index(['Date'])
 
 #Re-sampling to 10min & re-filling, with linear interpolation
-idx = pd.date_range('2016-09-11', '2018-03-24 23:50', freq = '10T')
+idx = pd.date_range('2016-09-11', '2018-04-05 23:00', freq = '60T')
 zones = zones.reindex(idx).interpolate(method = 'linear')
+#zones = zones.reindex(idx).fillna(method = 'pad', limit = 1)
 
 #  Merging tables in one data frame, 'df'
-df = pd.DataFrame()
-df = zones.merge(zones, left_index = True, right_index = True)
+#zones = zones.columns[0:11]
 
+df = pd.DataFrame()
+df = zones
+#df = zones#.merge(zones, left_index = True, right_index = True)
 
 #  Recovering 'Date' as data with new name, 'tStart'
 df = df.reset_index()
@@ -60,87 +50,63 @@ df = df.rename(columns = {'index':'tStart'})
 
 df['setPoint'] = setPoint
 df['clasificador'] = 1
-
-#print(df.count)
-
-df_changes = df.drop(['tStart', 'setPoint', 'clasificador'], 1)
-
-#
-#print(df_changes.dtypes)
+df_changes = df.drop(['setPoint', 'clasificador', 'tStart'], 1)
 
 beforeMean = 0
-countForFails = 1
-countForFails2 = 1
 clasificacion = []
-#setpoinst = [20.3, 15.6, 27.5, 18.9]
-#aux = 0
-#setPoint = randint(0, 3)
-"""for indice_fila, fila in df.iterrows():
-    if aux >= 72:
-        aux = 0
-        setPoint = randint(0, 3)
-    df.iloc[indice_fila, -2] = setpoinst.__getitem__(setPoint)
-    aux = aux+1
-
-print(df.head())"""
-
-
 
 for indice_fila, fila in df_changes.iterrows():
     if indice_fila == 0:
-        fila.abs()
+        #fila.abs()
         meanRow = fila.mean()
         beforeMean = meanRow
         clasificacion.append(1)
 
     if indice_fila > 0:
-        fila.abs()
+        #fila.abs()
         meanRow = stats.mean(fila)
         if beforeMean >= setPoint:
             #print(meanFila)
-            if ((meanRow < beforeMean) & (countForFails == 1)):
+            if (meanRow < beforeMean): #& (countForFails == 1)):
                 clasificacion.append(1)
                 beforeMean = meanRow
-                countForFails = 1
-            elif ((meanRow >= beforeMean) & (countForFails == 1)):
-                clasificacion.append(1)
-                beforeMean = beforeMean
-                countForFails = countForFails + 1    
-            elif ((meanRow >= beforeMean) & (countForFails == 2)):
+                #countForFails = 1
+            elif (meanRow >= beforeMean): #& (countForFails == 1)):
+                clasificacion.append(0)
+                beforeMean = meanRow
+                #countForFails = countForFails + 1    
+            """elif ((meanRow >= beforeMean) & (countForFails == 2)):
                 clasificacion.append(0)
                 countForFails = 1
                 beforeMean = meanRow
             elif ((meanRow < beforeMean) & (countForFails ==2)):
                 countForFails = 1
                 beforeMean = meanRow
-                clasificacion.append(0) 
+                clasificacion.append(0)""" 
         elif beforeMean < setPoint:
             #print(meanFila)
-            if ((meanRow > beforeMean) & (countForFails == 1)):
+            if (meanRow > beforeMean): #& (countForFails == 1)):
                 clasificacion.append(1)
                 beforeMean = meanRow
-                countForFails = 1
-            elif ((meanRow <= beforeMean) & (countForFails == 1)):
-                clasificacion.append(1)
+                #countForFails = 1
+            elif (meanRow <= beforeMean): #& (countForFails == 1)):
+                clasificacion.append(0)
                 beforeMean = meanRow
-                countForFails = countForFails + 1    
-            elif ((meanRow <= beforeMean) & (countForFails == 2)):
+                #countForFails = countForFails + 1    
+            """elif ((meanRow <= beforeMean) & (countForFails == 2)):
                 clasificacion.append(0)
                 countForFails = 1
                 beforeMean = meanRow 
             elif ((meanRow > beforeMean) & (countForFails ==2)):
                 countForFails = 1
                 beforeMean = meanRow
-                clasificacion.append(0) 
+                clasificacion.append(0)""" 
 
 
 for indice_fila, fila in df.iterrows():
     df.iloc[indice_fila, -1] = clasificacion.__getitem__(indice_fila)
 
 
-#print(df.head())
-#print(df_changes.count)
-#print(clasificacion)
 writer = ExcelWriter('clasificasion.xlsx')
 df.to_excel(writer, 'Hoja de datos', index=False)
 writer.save()
--- LogisticRegression/LogisticRegressionModel.py
+++ LogisticRegression/LogisticRegressionModel.py
@@ -4,13 +4,13 @@ from sklearn import linear_model
 from sklearn import model_selection
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 import matplotlib.pyplot as plt
 import seaborn as sb
 #%matplotlib inline
 
-dataframe = pd.read_excel('./inputs/ejemplo.xlsx')
-#dataframe.head()
+dataframe = pd.read_excel('./input/clasificasion.xlsx')
+#print(dataframe.head())
 #print(dataframe.head())
 #dataframe.describe()
 	
@@ -25,22 +25,42 @@ dataframe = pd.read_excel('./inputs/ejemplo.xlsx')
 #sb.pairplot(dataframe.dropna(), hue='clasificador', height=8,vars=["s_TExt_SWC", "s_TExt_NWF","s_Tr_AmbC","s_TRet_AmbF",
 #"h_Cap_ChF", "h_Cap_ChC", "h_Cap_Ch1", "h_Cap_Ch2"], kind='reg')
 
-#Eliminacion de datos no necesatios del Data Frame
-dataframe1 = dataframe.drop(['Date'], 1)
+#Eliminacion de datos no necesarios del Data Frame
+#dataframe1 = dataframe.drop(['tStart'], 1)
+#print(dataframe1.head())
 #Construccion de "X" (independiente) y "y" (dependiente) 
-X = np.array(dataframe1.drop(['clasificador'],1))
-y = np.array(dataframe['clasificador'])
+dfm = dataframe.drop(['clasificador', 'tStart'], 1)
+#   print(dfm.head())
+X = dfm
+y = dataframe['clasificador']
 
 #comprobacion de dimension
 #print(X.shape)
 
 #construccion del modelo de regresion
-model = linear_model.LogisticRegression()
-model.fit(X,y)
+model = linear_model.LogisticRegression(max_iter=500)
+model = model.fit(X,y)
 
 #Prediccion acorde a los datos anteriores
-predictions = model.predict(X)
-print(predictions)#[0:5]
+#predictions = model.predict(X)
+#print(predictions[:])#[0:5]
 #print(dataframe['clasificador'])
 
-print(model.score(X,y))
+#print(model.score(X,y))
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, y, test_size=validation_size, random_state=seed)
+
+name='Logistic Regression'
+kfold = model_selection.KFold(n_splits=10, random_state=seed)
+cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+print(msg)
+
+predictions = model.predict(X_validation)
+print('\n')
+print(accuracy_score(Y_validation, predictions))
+print('\n')
+print(confusion_matrix(Y_validation, predictions))
+
+print(f1_score(Y_validation, predictions, average='weighted'))

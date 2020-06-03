ae91f357018509a56fa06ad39fec7dadb360646b
Daniel <damesaa@eafit.edu.co>
2020-06-03 16:46:32 -0500

    update

--- api/DataBase.py
+++ api/DataBase.py
@@ -1,5 +1,7 @@
 import pymongo
 from pymongo import MongoClient
 
-conexion = MongoClient('localhost', 27017)
-db = conexion.dataIntegrador
+#conexion = MongoClient('localhost', 27017)
+#db = conexion.dataIntegrador
+client = pymongo.MongoClient("mongodb+srv://dbuser:integrador@cluster0-g9e4u.mongodb.net/dataIntegrador?retryWrites=true&w=majority")
+db = client.dataIntegrador

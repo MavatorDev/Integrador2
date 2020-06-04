f3db4ea148b07ee02e8f0a35648e70f761c4f615
Ubuntu <ubuntu@ip-172-31-56-54.ec2.internal>
2020-06-04 18:45:01 +0000

    changes

--- dashboard/server/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const user = require('../controllers/usuario.controller');
-
-//Usuarios
-router.post('/registerADMT', user.createUser);
-router.post('/login', user.userLogin);
-
-module.exports = router;

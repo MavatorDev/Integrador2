4735be54afac8fb56d772877e475a7c0cf68f2fe
eafit-201710143010 <asanchezc@eafit.edu.co>
2020-06-04 02:00:28 -0500

    mejora optimización y servicios

--- api/models/optimization.py
+++ api/models/optimization.py
@@ -17,3 +17,27 @@ def saveSolution(dic):
     except Exception as e:
         print ("Unexpected error:", type(e), e)
 
+def resultsExecuteModel(n):
+    result = {"Date": datetime.now(), "state": n}
+    try:
+        resultModel.insert_one(result)
+    except Exception as e:
+        print ("Unexpected error:", type(e), e)
+
+def getResults():
+    curs= resultModel.find()
+    list = []
+    for fut in curs:
+        list.append({"Date ": fut['Date'] , "state": fut['state']})
+    list.reverse()
+    list = list[0:10]
+    return list
+
+def getSolutions():
+    curs= op.find()
+    list = []
+    for fut in curs:
+        list.append({"bomba1": fut["bomba1"], "bomba2": fut["bomba2"], "bomba3": fut["bomba3"], "bomba4": fut["bomba4"]})
+    list.reverse()
+    list = list[0:6]
+    return list
